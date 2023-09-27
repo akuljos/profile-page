c4669ef12235a707fa8019849369fd79ac06eac0
Akul Joshi <joshi.akul@gmail.com>
2023-09-27 15:53:43 -0500

    trying out react-bootstrap

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,20 +1,32 @@
+import React from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Experience from './components/Experience.js';
-import Projects from './components/Projects.js';
-import Home from './components/Home.js';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/experience" element={ <Experience /> } />
-        <Route path="/projects" element={ <Projects /> } />
-      </Routes>
-    </div>
-  );
+  return(
+      <div className="App">
+        <Navbar expand="lg" className="bg-success-subtle">
+          <Container>
+            <Navbar.Brand href="#home">Akul Joshi</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#education">Education</Nav.Link>
+                <Nav.Link href="#experience">Experience</Nav.Link>
+                <Nav.Link href="#Projects">Projects</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+     </div>
+  )
 }
 
 export default App;
